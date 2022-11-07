57fe6461365e6bc39d70a817b4786001ae0e40bd
copperbulk <117660870+copperbulk@users.noreply.github.com>
2022-11-07 15:47:04 +0100

    Create types.py

---
+++ types.py
@@ -0,0 +1,8 @@
+# Sled / Default format (FM7)
+default_format = '<iI27f4i20f5i'
+
+# Dash format (FM7)
+dash_format = default_format + '17fH6B3b'
+
+# Horizon format (FH4+)
+horizon_format = default_format + 'i19fH6B4b'
